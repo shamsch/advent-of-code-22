b1ac8995639c2ea29454feddd46348b0ab22da2e
Shamsur Raza Chowdhury <shamsur314@gmail.com>
2022-12-05 19:15:22 +0200

    solved problem1

--- day5/problem1/main.go
+++ day5/problem1/main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strconv"
 	"strings"
-
 	"github.com/shamsch/advent-of-code-22/helper"
 )
+
 var positionWithCrates = []string{"1", "5", "9", "13", "17", "21", "25", "29", "33"} 
+
+type position struct {
+	crateNumber int
+	crate string
+}
 
 func parseMovingInstruction(instruction string) (string, string, string) {
 	instructionParts := strings.Split(instruction, " ")
@@ -15,6 +21,39 @@ func parseMovingInstruction(instruction string) (string, string, string) {
 	from := instructionParts[3]
 	to := instructionParts[5]
 	return amount, from, to
+}
+
+func moveTheCrates(amount int, from string, to string, cratesAndPositions map[string][]string) {
+	crates := helper.CloneASlice((cratesAndPositions)[from])
+	// remove the crates from the from position
+	// take from start of the slice
+	(cratesAndPositions)[from] = crates[amount:]
+	// add the crates to the to position
+	// append to the beginning of the slice
+	(cratesAndPositions)[to] = append(helper.ReverseSlice(helper.CloneASlice(crates[:amount])), (cratesAndPositions)[to]...)
+}
+
+func getTheTopMostCrate(cratesAndPositions map[string][]string) string {
+	topMostCrate := []position{}
+	topMostCrateAsString := ""
+
+	// get the top most crate
+	for idx, val := range cratesAndPositions {
+		crateNumber, _ := strconv.Atoi(idx)
+		topMostCrate = append(topMostCrate, position{crateNumber, val[0]})
+	}
+	
+	// sort the the positions of the crates in ascending order
+	sort.Slice(topMostCrate, func(i, j int) bool {
+		return topMostCrate[i].crateNumber < topMostCrate[j].crateNumber
+	})
+
+	// append the top most crate to the string
+	for _, val := range topMostCrate {
+		topMostCrateAsString += val.crate
+	}
+
+	return topMostCrateAsString
 }
 
 func main() {
@@ -26,7 +65,7 @@ func main() {
 		if reachedEnd {
 			amount, from, to := parseMovingInstruction(line)
 			amountInt, _ := strconv.Atoi(amount)
-			fmt.Println(amountInt, from, to)
+			moveTheCrates(amountInt, from, to, cratesAndPositions)
 		}
 		if line == "" {
 			reachedEnd = true  
@@ -36,14 +75,15 @@ func main() {
 			for idx, val := range line{
 				// if the current position is a crate position
 				if helper.SliceContains(positionWithCrates, strconv.Itoa(idx)) {
-					cratesAndPositions[strconv.Itoa(cratePosition)] = append(cratesAndPositions[strconv.Itoa(cratePosition)], string(val))
+					if string(val) != " "{
+						cratesAndPositions[strconv.Itoa(cratePosition)] = append(cratesAndPositions[strconv.Itoa(cratePosition)], string(val))
+					}	
 					cratePosition++
 				}
 			}
 		}
 		
 	}
-	for key, val := range cratesAndPositions {
-		fmt.Println(key, val)
-	}
+	
+	fmt.Println(getTheTopMostCrate(cratesAndPositions))
 }
