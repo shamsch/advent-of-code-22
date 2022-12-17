247f38a1be7d5f93f297f57c995a8559a8bedf89
Shamsur <shamsur314@gmail.com>
2022-12-17 20:02:58 +0200

    value array prints

--- day_10/problem/main.go
+++ day_10/problem/main.go
@@ -2,12 +2,28 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
+
 	"github.com/shamsch/advent-of-code-22/helper"
 )
 
 func main() {
 	lines := helper.ReadInputFileLines("day_10/input.txt")
-	fmt.Println(lines)
+	valueArray := []int{}
+	value := 1 
+	for _, line := range lines {
+		if line =="noop"{
+			valueArray = append(valueArray, value)
+		} else {
+			splitline := strings.Split(line, " ")
+			valueArray = append(valueArray, value)
+			valueArray = append(valueArray, value)
+			increment, _ := strconv.Atoi(splitline[1])
+			value += increment
+		}
+	}
+	fmt.Println(valueArray)
 }
 
 func calculateValue()  {
