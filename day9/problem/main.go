2359545a41cdda088692fe000294ccc287157524
Shamsur <shamsur314@gmail.com>
2022-12-18 14:13:32 +0200

    initial day9

---
+++ day9/problem/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/shamsch/advent-of-code-22/helper"
+)
+
+func main() {
+	lines := helper.ReadInputFileLines("day9/input.txt")
+	fmt.Println(lines)
+
+}
